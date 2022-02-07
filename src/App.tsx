9539111d61ab64c2b2109e16152ea0aea97791a7
JiyeonMin <ichbinmin2@gmail.com>
2022-02-07 23:31:39 +0900

    Add : App component에 Global style 적용 완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
+import styled, { ThemeProvider } from "styled-components";
+import GlobalStyle from "./styles/global-styles";
+import GlobalFont from "./styles/global-font";
+import { theme } from "./styles/theme";
+
 function App() {
-  return <h1>Hello :) </h1>;
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <GlobalFont />
+      <Div></Div>
+      <div className="App">hello world~!</div>
+    </ThemeProvider>
+  );
 }
 
 export default App;
+
+const Div = styled.div`
+  border: none;
+  // 이런 식으로 theme울 props 받아서 사용하면 됩니다.
+  background-color: ${({ theme }) => theme.color.red};
+  color: white;
+`;
