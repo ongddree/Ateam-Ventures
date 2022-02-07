44293153b29172958a01e08c0fb375251c8ee21a
JiyeonMin <ichbinmin2@gmail.com>
2022-02-08 04:55:42 +0900

    Modify : theme color 수정

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -4,14 +4,15 @@ const theme: DefaultTheme = {
   // basicWidth: "320px",
 
   color: {
-    WHITE : "#ffffff",
-    BLACK : "#000000",
-    BLUE : "#1565C0", // header
-    SKY : "#2196F3", // button
-    GRAY : "#E5E5E5", // border
-    DARKGRAY : "#C2C2C2", // offToggle
-    LIGHTBLUE : "#2196F3", // onToggle
-    ORANGE : "#FFA000", // orangebutton
+    WHITE: "#ffffff",
+    BLACK: "#000000",
+    BLUE: "#1565C0", // header
+    SKY: "#2196F3", // button
+    GRAY: "#E5E5E5", // border
+    DARKGRAY: "#C2C2C2", // offToggle
+    LIGHTBLUE: "#2196F3", // onToggle
+    ORANGE: "#FFA000", // orangebutton
+    FONTCOLOR: "#323D45", // main board font color
   },
 };
 
