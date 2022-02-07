758425bec33515102b8316e7480dc5701a3e33bc
dahye-program <dahye0729@sch.ac.kr>
2022-02-08 02:18:09 +0900

    Add : 이미지 export 파일 추가

---
+++ src/assets/image/index.ts
@@ -0,0 +1,6 @@
+export { default as MenuLOGO} from './menulogo.png';
+export { default as TitleLOGO} from './titlelogo.png';
+export { default as TitleMobileLOGO} from './titlelogomobile.png';
+export { default as VectorLOGO} from './vectorlogo.png';
+export { default as VectorMobileLOGO} from './vectorlogomobile.png';
+ 
