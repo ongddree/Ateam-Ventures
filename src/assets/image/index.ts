6961286395ce200a8e653f76bb5328dd9eae0672
dahye-program <dahye0729@sch.ac.kr>
2022-02-08 16:41:41 +0900

    Modify : 오타 수정

--- src/assets/image/index.ts
+++ src/assets/image/index.ts
@@ -4,4 +4,4 @@ export { default as TitleMobileLOGO } from '@/assets/image/titlelogomobile.png';
 export { default as VectorLOGO } from '@/assets/image/vectorlogo.png';
 export { default as VectorMobileLOGO } from '@/assets/image/vectorlogomobile.png';
 export { default as Arrowndown } from '@/assets/image/arrowdropdown.svg';
-export { default as ResetLOGO } from '@/assets/image/refershlogo.png';
+export { default as ResetLOGO } from '@/assets/image/refreshlogo.png';
