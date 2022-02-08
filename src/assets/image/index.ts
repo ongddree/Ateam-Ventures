7b1534a1e6b5135fac3cf178a3dbd6dadf0e6776
seoyoung park <ongddree@gmail.com>
2022-02-08 16:08:47 +0900

    Style : 드롭다운 화살표 svg 추가

--- src/assets/image/index.ts
+++ src/assets/image/index.ts
@@ -1,6 +1,6 @@
-export { default as MenuLOGO} from '@/assets/image/menulogo.png';
-export { default as TitleLOGO} from '@/assets/image/titlelogo.png';
-export { default as TitleMobileLOGO} from '@/assets/image/titlelogomobile.png';
-export { default as VectorLOGO} from '@/assets/image/vectorlogo.png';
-export { default as VectorMobileLOGO} from '@/assets/image/vectorlogomobile.png';
- 
+export { default as MenuLOGO } from '@/assets/image/menulogo.png';
+export { default as TitleLOGO } from '@/assets/image/titlelogo.png';
+export { default as TitleMobileLOGO } from '@/assets/image/titlelogomobile.png';
+export { default as VectorLOGO } from '@/assets/image/vectorlogo.png';
+export { default as VectorMobileLOGO } from '@/assets/image/vectorlogomobile.png';
+export { default as Arrowndown } from '@/assets/image/arrowdropdown.svg';
